8bff989b12ba195fa5a454b07aa64918bb0c55a3
sheila <sheilapg26@gmail.com>
2022-02-14 15:56:37 +0100

    Exercici1

--- src/file.js
+++ src/file.js
@@ -1,36 +1,10 @@
-console.log("empieza");
-fetch('https://icanhazdadjoke.com/', {
-    method: 'GET',
-    headers: {
-        'Accept': 'application/json'
-    }
-})
-    .then(function (response) { return response.json(); })
-    .then(function (data) { return console.log(data); });
-console.log("acaba");
-/*
-export interface User {
-    name: string;
-    age: number;
+function btnNextJoke() {
+    fetch('https://icanhazdadjoke.com/', {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+        .then(function (response) { return response.json(); })
+        .then(function (data) { console.log(data.joke); })["catch"](function (error) { return console.log(error); });
 }
-
-let myNumber: number = 0;
-let myString: string = "asdfsdf";
-let isTrue: boolean = false;
-let myNull: null = null;
-let myUndefined: undefined = undefined;
-
-let myStringArrary: Array<string> = ["primero", "segundo"];
-let myNumbersArray: Array<any> = [{}];
-
-
-let myObject: User = {
-    name: "Sheila",
-    age: 23
-}
-
-function myFunction(age: number): void { }
-
-function myTrueFunction(): boolean {
-    return true;
-}*/
--- src/file.ts
+++ src/file.ts
@@ -1,4 +1,14 @@
-
+function btnNextJoke() :void {
+    fetch('https://icanhazdadjoke.com/', {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+        .then((response) => response.json())
+        .then(data => { console.log(data.joke) })
+        .catch(error => console.log(error))
+}
 
 
 
