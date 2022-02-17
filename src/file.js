858c34affb0952ef9eea5624b65b6fb660217a7f
sheila <sheilapg26@gmail.com>
2022-02-17 17:01:46 +0100

    Exercici5

--- src/file.js
+++ src/file.js
@@ -1,7 +1,8 @@
 var currentJoke;
 var reportJokes = [];
 var date = new Date();
-function btnNextJoke() {
+var randomJoke;
+function randomJokes() {
     fetch('https://icanhazdadjoke.com/', {
         method: 'GET',
         headers: {
@@ -10,16 +11,33 @@ function btnNextJoke() {
     })
         .then(function (response) { return response.json(); })
         .then(function (data) {
+        randomJoke = "randomJokes";
         reportJokes.push({
             joke: data.joke,
             score: 0,
             date: date.toISOString()
         });
         currentJoke = data.joke;
-        console.log(data.joke);
         var joke = document.getElementById('jokes');
         joke.innerHTML = "\n            <p>".concat(data.joke, "</p>\n            <div class=\"pointsBtns mb-4\">\n                <button type=\"button\" onclick=\"sumPoints(1)\" class=\"pointBtn me-3 rounded\"><img class=\"starBtn pb-1\" src=\"/images/estrella.png\" alt=\"\"></button>\n                <button type=\"button\" onclick=\"sumPoints(2)\" class=\"pointBtn me-3 rounded\"><img class=\"starBtn pb-1\" src=\"/images/estrella.png\" alt=\"\"><img class=\"starBtn pb-1\" src=\"/images/estrella.png\" alt=\"\"></button>\n                <button type=\"button\" onclick=\"sumPoints(3)\" class=\"pointBtn me-3 rounded\"><img class=\"starBtn pb-1\" src=\"/images/estrella.png\" alt=\"\"><img class=\"starBtn pb-1\" src=\"/images/estrella.png\" alt=\"\"><img class=\"starBtn pb-1\" src=\"/images/estrella.png\" alt=\"\"></button>\n            </div>\n            ");
     })["catch"](function (error) { return console.log(error); });
+}
+function randomChuckJokes() {
+    fetch('https://api.chucknorris.io/jokes/random')
+        .then(function (response) { return response.json(); })
+        .then(function (data) {
+        randomJoke = "randomChuckJokes";
+        var chuckJoke = document.getElementById('jokes');
+        chuckJoke.innerHTML = "\n        <p>".concat(data.value, "</p>\n        <div class=\"pointsBtns mb-4\">\n            <button type=\"button\" onclick=\"sumPoints(1)\" class=\"pointBtn me-3 rounded\"><img class=\"starBtn pb-1\" src=\"/images/estrella.png\" alt=\"\"></button>\n            <button type=\"button\" onclick=\"sumPoints(2)\" class=\"pointBtn me-3 rounded\"><img class=\"starBtn pb-1\" src=\"/images/estrella.png\" alt=\"\"><img class=\"starBtn pb-1\" src=\"/images/estrella.png\" alt=\"\"></button>\n            <button type=\"button\" onclick=\"sumPoints(3)\" class=\"pointBtn me-3 rounded\"><img class=\"starBtn pb-1\" src=\"/images/estrella.png\" alt=\"\"><img class=\"starBtn pb-1\" src=\"/images/estrella.png\" alt=\"\"><img class=\"starBtn pb-1\" src=\"/images/estrella.png\" alt=\"\"></button>\n        </div>\n        ");
+    })["catch"](function (error) { return console.log(error); });
+}
+function btnNextJoke() {
+    if (randomJoke === "randomJokes") {
+        randomChuckJokes();
+    }
+    else {
+        randomJokes();
+    }
 }
 function sumPoints(point) {
     reportJokes = reportJokes.map(function (saveJoke) {
@@ -33,7 +51,6 @@ function sumPoints(point) {
 fetch('http://api.openweathermap.org/data/2.5/weather?lat=41&lon=2&appid=a368acc4079e173d9d9dd14dbc666c54')
     .then(function (response) { return response.json(); })
     .then(function (data) {
-    console.log(data);
     var weather = document.getElementById('weather');
     weather.innerHTML = "\n        <p>".concat(data.weather[0].description, "</p>\n        <img src=\"https://openweathermap.org/img/wn/").concat(data.weather[0].icon, ".png\">");
 })["catch"](function (error) { return console.log(error); });
--- src/file.ts
+++ src/file.ts
@@ -6,8 +6,9 @@ interface Joke {
 let currentJoke;
 let reportJokes: Joke[] = [];
 const date = new Date();
+let randomJoke;
 
-function btnNextJoke(): void {
+function randomJokes() {
     fetch('https://icanhazdadjoke.com/', {
         method: 'GET',
         headers: {
@@ -16,13 +17,13 @@ function btnNextJoke(): void {
     })
         .then((response) => response.json())
         .then(data => {
+            randomJoke = "randomJokes";
             reportJokes.push({
                 joke: data.joke,
                 score: 0,
                 date: date.toISOString()
             });
             currentJoke = data.joke;
-            console.log(data.joke)
             let joke = document.getElementById('jokes') 
             joke.innerHTML = `
             <p>${data.joke}</p>
@@ -35,6 +36,34 @@ function btnNextJoke(): void {
          })
         .catch(error => console.log(error))
 }
+
+function randomChuckJokes() {
+    fetch('https://api.chucknorris.io/jokes/random')
+    .then((response) => response.json())
+        .then((data) => {
+        randomJoke = "randomChuckJokes";
+        let chuckJoke = document.getElementById('jokes') 
+        chuckJoke.innerHTML = `
+        <p>${data.value}</p>
+        <div class="pointsBtns mb-4">
+            <button type="button" onclick="sumPoints(1)" class="pointBtn me-3 rounded"><img class="starBtn pb-1" src="/images/estrella.png" alt=""></button>
+            <button type="button" onclick="sumPoints(2)" class="pointBtn me-3 rounded"><img class="starBtn pb-1" src="/images/estrella.png" alt=""><img class="starBtn pb-1" src="/images/estrella.png" alt=""></button>
+            <button type="button" onclick="sumPoints(3)" class="pointBtn me-3 rounded"><img class="starBtn pb-1" src="/images/estrella.png" alt=""><img class="starBtn pb-1" src="/images/estrella.png" alt=""><img class="starBtn pb-1" src="/images/estrella.png" alt=""></button>
+        </div>
+        `
+        })
+        .catch(error => console.log(error))
+}
+
+function btnNextJoke(): void { 
+    if (randomJoke === "randomJokes") {
+        randomChuckJokes()
+    } else {
+        randomJokes()    
+    }
+ 
+}
+
 
 function sumPoints(point: number) {
     reportJokes = reportJokes.map((saveJoke: Joke) => { 
@@ -49,7 +78,8 @@ function sumPoints(point: number) {
 
 fetch('http://api.openweathermap.org/data/2.5/weather?lat=41&lon=2&appid=a368acc4079e173d9d9dd14dbc666c54')
     .then((response) => response.json())
-    .then((data) => { console.log(data)
+    .then((data) => {
+
         let weather = document.getElementById('weather');
         weather.innerHTML = `
         <p>${data.weather[0].description}</p>
